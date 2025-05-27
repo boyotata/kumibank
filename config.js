97672473578b0838592b3e52ea49a76cef19abd6
thomas <92831135+boyotata@users.noreply.github.com>
2025-05-27 16:43:29 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-2_vQQ8bYFSz', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1j2705tg9ijdvtlb13p3nuc2na', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv,
+        userPoolId: 'eu-west-2_0RtlhCExw', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4ujr61utvphifsckjftti6ofok', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv,
         
         region: 'eu-west-2' // e.g. us-east-2
     },
